9b400508035b7f29c7247d355a6fc95cf6a412ee
sage-yh <solitarysage.ea@gmail.com>
2024-03-14 19:45:59 +0000

    I

--- GoHeartArt.go
+++ GoHeartArt.go
@@ -3,24 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	printBoxWithRedHeartAndText("I am looking for you")
-}
-
-func printBoxWithRedHeartAndText(text string) {
-	// Print top of the box
-	fmt.Println("+++++++++++++++++++++++++++++")
-
-	// Print sides of the box
-	for i := 0; i < 5; i++ {
-		if i == 2 {
-			fmt.Println("|       ❤️                   |")
-		} else if i == 3 {
-			fmt.Println("|", text, "|")
-		} else {
-			fmt.Println("|                             |")
-		}
-	}
-
-	// Print bottom of the box
-	fmt.Println("+++++++++++++++++++++++++++++")
+	// Square shape with a heart inside it and the letter I
+	fmt.Println("***********")
+	fmt.Println("*    ♥    *")
+	fmt.Println("*   ♥♥♥   *")
+	fmt.Println("*  ♥♥♥♥♥  *")
+	fmt.Println("* ♥♥♥♥♥♥♥ *")
+	fmt.Println("*♥♥♥♥I♥♥♥♥*")
+	fmt.Println("* ♥♥♥♥♥♥♥ *")
+	fmt.Println("*  ♥♥♥♥♥  *")
+	fmt.Println("*   ♥♥♥   *")
+	fmt.Println("*    ♥    *")
+	fmt.Println("***********")
 }
